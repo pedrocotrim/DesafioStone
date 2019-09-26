6e9f35ea0aba52dc02ec70f0dce2b21856e3270e
Pedro Cotrim <pacotrim@gmail.com>
2019-09-26 19:24:29 -0300

    Adiciona mailer no ambiente de produçao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,14 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "DesafioStone_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    :user_name => Rails.application.credentials.sendgrid[:username],
+    :password => Rails.application.credentials.sendgrid[:password],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
